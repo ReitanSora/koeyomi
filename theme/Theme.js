50fbd3b817f684cf6a086caca197d81e7101f1b2
Stiven <stivenpilca@gmail.com>
2025-03-07 19:42:12 -0500

    feat: add custom theme to application

---
+++ theme/Theme.js
@@ -0,0 +1,24 @@
+export const Theme = {
+    colors: {
+        white: '#FFFFFF',
+        lightWhite: '#F5F5F5',
+        black: '#121212',
+        vermillion: '#FF4500',
+        charcoalBlack: '#1E1E1E',
+        gunmetalGray: '#252525',
+        lightGray: '#E0E0E0',
+        midGray: '#8B8B8B',
+    },
+    borders: {
+        button: 20,
+        circle: '100%',
+        input: 7,
+        cardItem: 15,
+    },
+    fonts: {
+        title: 20,
+        subtitle: 15,
+        paragraph: 13,
+        tiny: 10,
+    },
+}
