d583008d5dbb97545aaa55767bee22e8907bca66
Stiven <stivenpilca@gmail.com>
2025-03-19 23:29:11 -0500

    feat: change backgroundColor

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './routes/Navigation';
+import { View } from 'react-native';
+import { Theme } from './theme/Theme';
 
 export default function App() {
   return (
     //<StatusBar animated={true} hidden={false} backgroundColor='#000'></StatusBar>
-    <Navigation />
+    <View style={{flex: 1, backgroundColor: Theme.colors.charcoalBlack}}>
+      <Navigation />
+    </View>
   );
 }
