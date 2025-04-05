8e880a640761ab582947ea7e89f80b2b7264acd5
Stiven <stivenpilca@gmail.com>
2025-04-05 17:14:11 -0500

    feat: add UI to display all information
    - Implement HeaderBackButton component 635464e c72ecb3.
    - Implement Carousel component e72da08.

--- screens/MangaReaderScreen/MangaReaderScreen.tsx
+++ screens/MangaReaderScreen/MangaReaderScreen.tsx
@@ -1,9 +1,57 @@
-import { View, Text } from 'react-native';
+import HeaderBackButton from '../../components/HeaderBackButton/HeaderBackButton';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect, useState } from 'react';
+import { fetcher } from '../../services/fetcher';
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import { styles } from './MangaReaderScreen.styles';
+import Carousel from '../../components/Carousel/Carousel';
 
-export default function MangaChapterScreen() {
-    return(
-        <View>
-            <Text>Esta es la pantalla de MangaChapterScreen</Text>
-        </View>
+export default function MangaReaderScreen({ route }: any) {
+
+    const { id, format, title, subtitle } = route.params;
+    const [imagesUrl, setImagesUrl] = useState<object>();
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    const headerStyle = useAnimatedStyle(() => {
+        return {
+            opacity: isMenuVisible
+                ? withTiming(1, { duration: 200 })
+                : withTiming(0, { duration: 200 })
+        }
+    })
+
+    useEffect(() => {
+        const getImagesUrl = async () => {
+            const images = await fetcher(process.env.EXPO_PUBLIC_KOEYOMI_BACKEND, `/mangadex/chapter/${id}`) as object;
+            setImagesUrl(images);
+        }
+
+        getImagesUrl();
+    }, [])
+
+
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, position: 'relative' }}>
+                <Animated.View style={[styles.header, headerStyle]}>
+                    <HeaderBackButton
+                        hasFilter={false}
+                        hasDownloadOption={false}
+                        title={title}
+                        subtitle={subtitle}
+                        hidden={true} />
+                </Animated.View>
+
+                {imagesUrl &&
+                    <Carousel
+                        images={imagesUrl}
+                        format={format}
+                        onSingleTap={() => setIsMenuVisible(!isMenuVisible)}
+                        menuVisible={isMenuVisible} />
+                }
+
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 };
