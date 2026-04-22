3269f4aaf3c0ecd2560297a293fafeb00d023e45
ReitanSora <stivenpilca@gmail.com>
2026-04-21 21:03:42 -0500

    feat: add statusBar height constant

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,6 @@
 import { Dimensions } from "react-native";
+import Constants from 'expo-constants';
 
 export const { width: MAX_WIDTH, height: MAX_HEIGHT } = Dimensions.get('window');
 export const Padding = 10;
+export const statusBarHeight = Constants.statusBarHeight;
